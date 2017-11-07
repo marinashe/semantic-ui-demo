5101d2312733173f5cf1e593d802ae0d8830279f
Marina Sheenkova <marina@astrails.com>
2017-11-07 10:04:30 +0200

    fixed some eslint warnings

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,18 +28,29 @@ const HeaderApp = () => (
     </div>
     <div>
       <Button
-        color='red'
-        content='Like'
-        icon='heart'
-        label={{ basic: true, color: 'red', pointing: 'left', content: '2,048' }}
+        color="red"
+        content="Like"
+        icon="heart"
+        label={ {
+          basic: true,
+          color: 'red',
+          pointing: 'left',
+          content: '2,048'
+        } }
       />
 
       <Button
         basic
-        color='blue'
-        content='Fork'
-        icon='fork'
-        label={{ as: 'a', basic: true, color: 'blue', pointing: 'left', content: '1,048' }}
+        color="blue"
+        content="Fork"
+        icon="fork"
+        label={ {
+          as: 'a',
+          basic: true,
+          color: 'blue',
+          pointing: 'left',
+          content: '1,048'
+        } }
       />
     </div>
   </Header>
